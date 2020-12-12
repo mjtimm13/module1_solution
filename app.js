f46d3831482d9fbbcbe20d6a29caf6f8a0c44268
mjtimm13 <mjtimm@sbcglobal.net>
2020-12-11 20:34:12 -0600

    Updated web file.

--- app.js
+++ app.js
@@ -9,27 +9,44 @@ angular.module('LunchCheck', [])
   $scope.evalMessage = "";
 
   $scope.countItems = function () {
-    var totalItems = calculateNumericForString($scope.lunchList);
+    var totalItems = calculateItemsInString($scope.lunchList);
+    console.log(totalItems);
     $scope.itemCount = totalItems;
     var lunchMessage = evaluateList(totalItems);
     $scope.evalMessage = lunchMessage;
   };
 
 
-  function calculateNumericForString(string) {
-    var totalStringValue = 0;
-    for (var i = 0; i < string.length; i++) {
-      totalStringValue += string.charCodeAt(i);
-    }
+  function calculateItemsInString(string) {
+    var stringItems = 0;
+    var itemsArray = string.split(",");
 
-    return totalStringValue;
+    itemsArray.forEach(item => {
+      //if(item !== "" || item !== null){
+      if(item.trim().length > 0){
+            stringItems+=1;
+         }
+      }
+   )
+
+   console.log("Number of items="+stringItems);
+    console.log(itemsArray);
+
+    return stringItems;
   }
 
   function evaluateList(int) {
-    var lunchListMessage = "Please enter data first";
-    if (int > 1000) {
+    var lunchListMessage = "Please enter data first.";
+    if (int > 3) {
       lunchListMessage = "Too Much!";
-    }    /*for (var i = 0; i < string.length; i++) {
+    }
+    else if (int > 0) {
+      lunchListMessage = "Enjoy!"
+    }
+    else {
+      lunchListMessage = "Please enter data first";
+    }
+    /*for (var i = 0; i < string.length; i++) {
       totalStringValue += string.charCodeAt(i);
     }*/
 
